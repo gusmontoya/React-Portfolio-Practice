01075520e9e24fcaa5673f53764f4d551e57654c
gusmontoya <gus.montoya@gmail.com>
2021-12-11 22:44:23 -0800

    styled brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,19 +5,19 @@ import './brand.css';
 const Brand = () => (
   <div className="gpt3__brand section__padding">
     <div>
-      <img src={VSCode} height="35px" />
+      <img src={VSCode} height="45px" />
     </div>
     <div>
-      <img src={Figma} height="40px" />
+      <img src={Figma} height="45px" />
     </div>
     <div>
-      <img src={Github} height="35px" />
+      <img src={Github} height="45px" />
     </div>
     <div>
-      <img src={Git} height="35px" />
+      <img src={Git} height="55px" />
     </div>
     <div>
-      <img src={Affinity} height="35px" />
+      <img src={Affinity} height="45px" />
     </div>
     <div>
       <img src={Codepen} height="35px" />
--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -1,10 +1,10 @@
 
 import VSCode from '../../assets/visual-studio-code-logo-png.png';
 import Figma from '../../assets/figma-logo.png';
-import Github from '../../assets/github-white-logo-png-01.png';
-import Git from '../../assets/git-logo-text-logo.png';
+import Github from '../../assets/github_logo.png';
+import Git from '../../assets/git_logo.png';
 import Affinity from '../../assets/affinity-designer-png.png';
-import Codepen from '../../assets/codepen-logo-png-01.png';
+import Codepen from '../../assets/Codepen_logo.png';
 
 export {
   Codepen,
