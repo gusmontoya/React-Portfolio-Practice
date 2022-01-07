ae2868ac45063df43d4d8068539bdd1150e1af62
gusmontoya <gus.montoya@gmail.com>
2022-01-06 19:45:33 -0800

    fixed text in about section

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -13,14 +13,16 @@ const About = () => (
       <h1 className="gradient__text">Who is Gus?</h1>
       <p><span class="bold">I’m a non-traditional tech professional</span>. I am passionate with the evolving tools I use to complete projects that are current to todays demands and technology uses. 
           I'm a self-motivated learner with strong organisation, time management & communication skills. I'm able to work independently & collaboratively in a team with a meticulous attention to detail.
-          
+      </p>
+      <p>
           Before tech I gave 8 years to healthcare and home health. Adapting to Covids disruption I reskilled myself for a new path and fell in love with full stack development.
           As a follower of Robert Cecil Martin's <a className="anchorLink" href="https://youtu.be/7EmboKQH8lM" target="_blank" rel="noreferrer" aria-label="open link to Clean Code - Uncle Bob."> principles of clean code</a>,
           I agree that reducing complexity as much as possible is important in code.</p>
-          <p><span class="bold">I am a</span> 
+      <p><span class="bold">I am a</span> 
           <a className="anchorLink" href="https://www.nucamp.co" target="_blank" rel="noreferrer" aria-label="open link to Nucamp website."> Nucamp Coding Bootcamp</a> <span class="bold">graduate</span>.
       </p>
-      <p>I'm jubilant as a busy body learning, and creating digital content such as useful sites and secure backends. Solving elusive problems on 
+      <p>
+          I'm jubilant as a busy body learning, and creating digital content such as useful sites and secure backends. Solving elusive problems on 
           <a className="anchorLink" href="https://scrimba.com/" target="_blank" rel="noreferrer" aria-label="open link to scrimba's website."> scrimba </a>improving my skills daily and being active in the community on
           <a className="anchorLink" href="https://chingu.io/" target="_blank" rel="noreferrer" aria-label="open link to chingu's website."> chingu</a> and other coding oriented discord channels.
       </p>
