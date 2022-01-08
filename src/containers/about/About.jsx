eebfafd53915c15340e01f9f80ad853f50471e97
gusmontoya <gus.montoya@gmail.com>
2022-01-08 15:26:18 -0800

    edited about text

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -4,7 +4,7 @@ import about2Image from '../../assets/144.JPG';
 import './about.css';
 
 const About = () => (
-  <div className="gpt3__possibility section__padding" id="about">
+  <div className="gpt3__possibility section__margin" id="about">
     <div className="gpt3__possibility-image">
       <img src={about2Image} alt="possibility" border-radius="10px" />
     </div>
@@ -22,15 +22,15 @@ const About = () => (
           As a follower of Robert Cecil Martin's <a className="anchorLink" href="https://youtu.be/7EmboKQH8lM" target="_blank" rel="noreferrer" aria-label="open link to Clean Code - Uncle Bob."> principles of clean code</a>,
           I agree that reducing complexity as much as possible is important in code.</p>*/}
           {/* updated */}
-      <p><span class="bold">I am a </span> 
-          <a className="anchorLink" href="https://www.nucamp.co" target="_blank" rel="noreferrer" aria-label="open link to Nucamp website."> Nucamp Coding Bootcamp</a> <span class="bold">graduate</span>.
+      <p><span class="bold">I hold a Full Stack Developer Certification from</span> 
+          <a className="anchorLink" href="https://www.nucamp.co" target="_blank" rel="noreferrer" aria-label="open link to Nucamp website."> Nucamp Coding Bootcamp</a> <span class="bold">where I became proficient in BootStrap 5, React, NodeJS, Expres, and MongoDB. </span>.
       </p> 
-      <p>
+      {/* <p>
           I'm jubilant as a busy body learning and solving elusive problems on 
           <a className="anchorLink" href="https://scrimba.com/" target="_blank" rel="noreferrer" aria-label="open link to scrimba's website."> scrimba </a>improving my skills daily and being active in the community on
           <a className="anchorLink" href="https://chingu.io/" target="_blank" rel="noreferrer" aria-label="open link to chingu's website."> chingu</a> and other coding oriented discord channels.
-      </p>
-      <h4>Ah yes, I also enjoy a good ride (picture of my beauty on the left) and 3d printing.</h4>
+      </p> */}
+      {/* <h4>Ah yes, I also enjoy a good ride (picture of my beauty on the left) and 3d printing.</h4> */}
     </div>
   </div>
 );
